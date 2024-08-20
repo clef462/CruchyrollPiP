4abd208861ae0be45d3b8539cef875b118ec61e7
clef462 <35619752+clef462@users.noreply.github.com>
2024-08-20 06:57:29 -0500

    Delete RemovePiPBlocker directory

--- RemovePiPBlocker/content.js
+++
@@ -1,8 +0,0 @@
-const videos = document.querySelectorAll('video');
-
-videos.forEach((video) => {
-    console.log("Video found:", video);
-    video.removeAttribute('disablepictureinpicture');
-    video.setAttribute('allowPictureInPicture', '');
-    console.log("disablepictureinpicture removed and allowPictureInPicture set.");
-});
